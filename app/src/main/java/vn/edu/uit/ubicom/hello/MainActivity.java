2d998dd291dcdb437b7083b8ab585c7bc84889c5
Nguyen Anh Tuan <nguyenanhtuan@Andromeda.local>
2019-06-04 14:24:19 +0700

    2nd user adding second comment

--- app/src/main/java/vn/edu/uit/ubicom/hello/MainActivity.java
+++ app/src/main/java/vn/edu/uit/ubicom/hello/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Adding the first Comment 
+        // 2nd user adding 2nd comment 
+        int i=0; 
+
     }
 }
